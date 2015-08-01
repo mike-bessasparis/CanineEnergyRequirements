184de2126c7e81a6fd68918c251614e5a6000ce3
Michael Bessasparis <michael.bessasparis@gmail.com>
2015-08-01 15:20:08 -0500

    implemented "about" menu choice.

---
+++ app/src/main/java/com/bessasparis/mike/canineenergyrequirements/ActionsAbout.java
@@ -0,0 +1,15 @@
+package com.bessasparis.mike.canineenergyrequirements;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class ActionsAbout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actions_about);
+    }
+
+}
--- app/src/main/java/com/bessasparis/mike/canineenergyrequirements/MainActivity.java
+++ app/src/main/java/com/bessasparis/mike/canineenergyrequirements/MainActivity.java
@@ -1,5 +1,6 @@
 package com.bessasparis.mike.canineenergyrequirements;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
@@ -141,11 +142,16 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.action_settings: {
+                return true;
+            }
+            case R.id.action_about: {
+                Intent intent = new Intent(this, ActionsAbout.class);
+                startActivity(intent);
+                return true;
+            }
         }
 
         return super.onOptionsItemSelected(item);
