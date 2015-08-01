df24f13d24021b7d8d05b0acacf32456ca1ed90f
Michael Bessasparis <michael.bessasparis@gmail.com>
2015-08-01 17:26:42 -0500

    basic spinner UI

--- app/src/main/java/com/bessasparis/mike/canineenergyrequirements/MainActivity.java
+++ app/src/main/java/com/bessasparis/mike/canineenergyrequirements/MainActivity.java
@@ -9,7 +9,9 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 
@@ -19,6 +21,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spinner = (Spinner) findViewById(R.id.basic_factors_spinner);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.basic_factors_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+
     }
 
     public void updateDER(View view) {
